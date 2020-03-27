df03c62d8a03e0197e2e856a83672f65c3892f53
Milosh_Bujakovich <milosbujakovic97@gmail.com>
2020-03-28 00:18:30 +0100

    Inicijalni komit

---
+++ ServisKlinickihCentara/src/main/java/com/ServisKlinickihCentara/ServisKlinickihCentaraApplication.java
@@ -0,0 +1,14 @@
+package com.ServisKlinickihCentara;	
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+public class ServisKlinickihCentaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisKlinickihCentaraApplication.class, args);
+	}
+}
