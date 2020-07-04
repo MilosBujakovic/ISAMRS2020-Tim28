35634ce0fcaaeee93e38f650967150371d5a7fb6
Milosh_Bujakovich <milosbujakovic97@gmail.com>
2020-07-04 21:40:04 +0200

    Fixed backend reference problem

--- ServisKlinickihCentara/src/main/java/com/ServisKlinickihCentara/controller/PredefinedAppointmentController.java
+++ ServisKlinickihCentara/src/main/java/com/ServisKlinickihCentara/controller/PredefinedAppointmentController.java
@@ -1,7 +1,5 @@
 package com.ServisKlinickihCentara.controller;
 
-import java.sql.Time;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +12,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ServisKlinickihCentara.dto.appointmentsDTO.AppointmentTermDTO;
-import com.ServisKlinickihCentara.model.clinics.TypeOfExam;
-import com.ServisKlinickihCentara.service.TypeOfExamService;
+import com.ServisKlinickihCentara.service.PredefinedAppointmentService;
 
 @RestController
 @RequestMapping( value = "/clinicAdmin/predefinedAppointment")
 public class PredefinedAppointmentController 
 {
-	@Autowired
-	TypeOfExamService examsService;
 	
 	@Autowired
 	PredefinedAppointmentService appointmentService;
@@ -30,25 +25,9 @@ public class PredefinedAppointmentController
 	@RequestMapping(value="/getTerms", method=RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<AppointmentTermDTO>> getTerms(@RequestBody AppointmentTermDTO appointmentSlot)
 	{
-		System.out.println("USaO U getTerms");
-		List<AppointmentTermDTO> terms = new ArrayList<AppointmentTermDTO>();
-		Time startTime = Time.valueOf(appointmentSlot.getStartTime() );
-		Time finalTime = Time.valueOf(appointmentSlot.getEndTime() );
-		int i = 0;
-		AppointmentTermDTO term = new AppointmentTermDTO();
-		TypeOfExam te = examsService.getExamType(appointmentSlot.getTypeOfExam());
-		do
-		{
-			i++;
-			System.out.println(startTime);
-			term.setStartTime(startTime.toString());
-			startTime.setMinutes(startTime.getMinutes()+te.getDuration());
-			term.setEndTime(startTime.toString());
-			System.out.println(startTime);
-			
-		}
-		while(startTime.before(finalTime));
 		
+		List<AppointmentTermDTO> terms = appointmentService.getTerms(appointmentSlot);
+		System.out.println("Free Appointment Terms delivered!");
 		return new ResponseEntity<List<AppointmentTermDTO>>(terms, HttpStatus.OK);
 	}
 }
--- ServisKlinickihCentara/src/main/java/com/ServisKlinickihCentara/service/PredefinedAppointmentService.java
+++ ServisKlinickihCentara/src/main/java/com/ServisKlinickihCentara/service/PredefinedAppointmentService.java
@@ -1,5 +1,48 @@
 package com.ServisKlinickihCentara.service;
 
-public class PredefinedAppointmentService {
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ServisKlinickihCentara.dto.appointmentsDTO.AppointmentTermDTO;
+import com.ServisKlinickihCentara.model.clinics.TypeOfExam;
+import com.ServisKlinickihCentara.repository.AppointmentRepository;
+
+@Service
+public class PredefinedAppointmentService 
+{
+
+	@Autowired
+	TypeOfExamService examsService;
+	
+	@Autowired
+	AppointmentRepository appointmentRepository;
+	
+	public List<AppointmentTermDTO> getTerms(AppointmentTermDTO appointmentSlot)
+	{
+		List<AppointmentTermDTO> terms = new ArrayList<AppointmentTermDTO>();
+		Time startTime = Time.valueOf(appointmentSlot.getStartTime() );
+		Time finalTime = Time.valueOf(appointmentSlot.getEndTime() );
+		int i = 0;
+		AppointmentTermDTO term = new AppointmentTermDTO();
+		TypeOfExam te = examsService.getExamType(appointmentSlot.getTypeOfExam());
+		do
+		{
+			term.setStartTime(startTime.toString());
+			startTime.setMinutes(startTime.getMinutes()+te.getDuration());
+			term.setEndTime(startTime.toString());
+			term.setTypeOfExam(appointmentSlot.getTypeOfExam());
+			terms.add(term);
+			System.out.println(terms.get(i).getStartTime() + "|" + terms.get(i).getEndTime() + "|" + terms.get(i).getTypeOfExam());
+			i++;
+			
+		}
+		while(startTime.before(finalTime));
+		
+		return terms;
+	}
+	
 }
